4c815be81c800fd4fdb720d33d52007296d77002
Peter Chiang <pccchiang@gmail.com>
2023-12-02 01:34:42 -0500

    Add template

---
+++ template.py
@@ -0,0 +1,20 @@
+def part1(f):
+    pass
+
+
+def part2(f):
+    pass
+
+
+def main():
+    filename = "day03.input"
+
+    with open(filename) as f:
+        print(part1(f))
+
+    with open(filename) as f:
+        print(part2(f))
+
+
+if __name__ == "__main__":
+    main()
