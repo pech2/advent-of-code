1d5b4bc28b7e7160ddf39f2898aa370844ce15b6
Peter Chiang <pccchiang@gmail.com>
2023-12-11 00:25:54 -0500

    Fix part 2 for day 11

--- 2023/day11/day11.py
+++ 2023/day11/day11.py
@@ -29,6 +29,52 @@ def part1(f):
                 distance += 1
                 row_a += 1
                 if row_a in rows:
+                    distance += 1
+
+            col_a, col_b = galaxy[1], pair[1]
+            if col_a > col_b:
+                col_a, col_b = col_b, col_a
+            while col_a < col_b:
+                distance += 1
+                col_a += 1
+                if col_a in cols:
+                    distance += 1
+
+            total += distance
+    return total
+
+
+def part2(f):
+    grid = []
+    for line in f:
+        grid.append(line.strip())
+    rows = set()
+    cols = set()
+    for i, row in enumerate(grid):
+        if all(cell == "." for cell in row):
+            rows.add(i)
+    for i, col in enumerate(zip(*grid)):
+        if all(cell == "." for cell in col):
+            cols.add(i)
+
+    galaxies = []
+    for row, line in enumerate(grid):
+        for col, cell in enumerate(line):
+            if cell == "#":
+                galaxies.append((row, col))
+
+    total = 0
+
+    for i, galaxy in enumerate(galaxies):
+        for pair in galaxies[i + 1 :]:
+            distance = 0
+            row_a, row_b = galaxy[0], pair[0]
+            if row_a > row_b:
+                row_a, row_b = row_b, row_a
+            while row_a < row_b:
+                distance += 1
+                row_a += 1
+                if row_a in rows:
                     distance += 999999
 
             col_a, col_b = galaxy[1], pair[1]
@@ -42,10 +88,6 @@ def part1(f):
 
             total += distance
     return total
-
-
-def part2(f):
-    pass
 
 
 def main():
