1b30ddfc43f35feb3dae60eae8c232f8fdcaba6f
Peter Chiang <pccchiang@gmail.com>
2024-12-06 00:42:22 -0500

    Remove unused variables

---
+++ 2023/day12/day12.py
@@ -0,0 +1,21 @@
+def part1(f):
+    pass
+
+
+def part2(f):
+    pass
+
+
+def main():
+    filename = "day12.example"
+    # filename = "day.input"
+
+    with open(filename) as f:
+        print(part1(f))
+
+    with open(filename) as f:
+        print(part2(f))
+
+
+if __name__ == "__main__":
+    main()
